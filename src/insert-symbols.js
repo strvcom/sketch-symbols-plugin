ca5e19e9e44d4cb4fc980bbda15cb763026a7305
PrimeStark <muqmuq@me.com>
2018-11-20 16:02:24 +0100

    Change set coordinates of new symbol without creating group

--- src/insert-symbols.js
+++ src/insert-symbols.js
@@ -4,47 +4,64 @@ import sketch from 'sketch' // eslint-disable-line
 export default function(symbols) {
   const document = sketch.Document.getSelectedDocument()
   const page = document.selectedPage
-  const selected = document.selectedLayers.map(layer => layer)
-  const selectedLayer = document.getLayerWithID(selected[0].id)
-  const selectedLayerParent = selected[0].parent
 
   function parentCheck() {
     if (document.selectedLayers.length > 0) {
-      if (selectedLayer.type !== 'Group') {
-        console.log(selectedLayer.parent.type)
-        // const coordinates = new sketch.Rectangle(selectedLayer.frame)
+      const selected = document.selectedLayers.map(layer => layer)
+      const selectedLayer = document.getLayerWithID(selected[0].id)
 
-        // const newGroup = new sketch.Group({
-        //   name: 'Created Layer',
-        //   parent: selectedLayerParent,
-        //   layers: [selectedLayer],
-        //   frame: coordinates,
-        // })
-        // selectedLayer.frame = new sketch.Rectangle(
-        //   0,
-        //   0,
-        //   coordinates.width,
-        //   coordinates.height
-        // )
-        return selectedLayerParent || page
+      if (selectedLayer.type === 'Group') {
+        return selectedLayer
       }
-      return selectedLayer
+      return selectedLayer.parent
     }
     return page
   }
 
+  function setCoordinates(master) {
+    if (document.selectedLayers.length > 0) {
+      const selected = document.selectedLayers.map(layer => layer)
+      const selectedLayer = document.getLayerWithID(selected[0].id)
+
+      if (selectedLayer.type === 'Group') {
+        return {
+          x: 0 + selectedLayer.frame.width / 2 - master.frame.width / 2,
+          y: 0 + selectedLayer.frame.height / 2 - master.frame.height / 2,
+        }
+      }
+
+      return {
+        x:
+          selectedLayer.frame.x +
+          selectedLayer.frame.width / 2 -
+          master.frame.width / 2,
+        y:
+          selectedLayer.frame.y +
+          selectedLayer.frame.height / 2 -
+          master.frame.height / 2,
+      }
+    }
+
+    return {
+      x: page.frame.x - master.frame.width / 2,
+      y: page.frame.y - master.frame.height / 2,
+    }
+  }
+
+  // const coordinates = setCoordinates()
   const symbolsParent = parentCheck()
 
   symbols.map(s => {
     const symbolMaster = document.getSymbolMasterWithID(s)
+    const coordinates = setCoordinates(symbolMaster)
 
     const newSymbolInstance = new sketch.SymbolInstance({
       parent: symbolsParent,
       symbolId: symbolMaster.symbolId,
       name: symbolMaster.name,
       frame: new sketch.Rectangle(
-        symbolsParent.frame.width / 2 - symbolMaster.frame.width / 2,
-        symbolsParent.frame.height / 2 - symbolMaster.frame.height / 2,
+        coordinates.x,
+        coordinates.y,
         symbolMaster.frame.width,
         symbolMaster.frame.height
       ),
